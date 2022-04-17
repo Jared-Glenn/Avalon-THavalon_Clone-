ff4d9d916da18193372e477a05085fbfe5bad4f1
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2022-04-16 22:48:22 -0600

    v 1.00
    Found roughly 30 bugs. Some were caused by the updated version of Python not being compatable. Corrected type errors and bugs. Program is now workable. Testing is likely to continue, but these improvements indicate a complete, working program.

--- Avalon.py
+++ Avalon.py
@@ -9,12 +9,12 @@ def get_role_description(role):
     return {
         'Tristan' : 'The person you see is also Good and is aware that you are Good.\nYou and Iseult are collectively a valid Assassination target.',
         'Iseult' : 'The person you see is also Good and is aware that you are Good.\nYou and Tristan are collectively a valid Assassination target.',
-        'Merlin' : 'You know which people have Evil roles, but not who has any specific role.\nYou are a valid Assassination target.'
+        'Merlin' : 'You know which people have Evil roles, but not who has any specific role.\nYou are a valid Assassination target.',
         'Percival' : 'You know which people have the Merlin and Morgana roles, but not who has each.',
         'Lancelot' : 'You may play Reversal cards while on missions.\nYou appear Evil to Merlin.',
         'Arthur' : 'You know which Good roles are in the game, but not who has any given role.\nIf two missions have Failed, and less than two missions have Succeeded, you may declare as Arthur.\nAfter declaring, your vote on team proposals is counted twice, but you are unable to be on mission teams until the 5th mission.\nAfter declaring, you are immune to any effect that can forcibly change your vote.',
         'Titania' : 'You appear as Evil to all players with Evil roles (except Colgrevance).',
-        'Nimue' : 'You know which Good and Evil roles are in the game, but not who has any given role.\You are a valid Assassination target.',
+        'Nimue' : 'You know which Good and Evil roles are in the game, but not who has any given role.\nYou are a valid Assassination target.',
 
         'Mordred' : 'You are hidden from all Good roles that could reveal that information.\nLike other Evil characters, you know who else is Evil (except Colgrevance).',
         'Morgana' : 'You appear like Merlin to Percival.\nLike other Evil characters, you know who else is Evil (except Colgrevance).',
@@ -29,23 +29,23 @@ def get_role_description(role):
 # - Arthur: no information
 # - Guinevere: too complicated to generate here
 # - Colgrevance: name, role (evil has an update later to inform them about the presence of Colgrevance)
-def get_role_information(myplayer,players):
+def get_role_information(my_player,players):
     return {
-        'Tristan' : ['{} is Iseult.'.format(player.name) for player in players if player.role is 'Iseult'],
-        'Iseult' : ['{} is Tristan.'.format(player.name) for player in players if player.role is 'Tristan'],
-        'Merlin' : ['{} is Evil'.format(player.name) for player in players if (player.team is 'Evil' and player.role is not 'Mordred') or player.role is 'Lancelot'],
-        'Percival' : ['{} is Merlin or Morgana.'.format(player.name) for player in players if player.role is 'Merlin' or player.role is 'Morgana'],
+        'Tristan' : ['{} is Iseult.'.format(player.name) for player in players if player.role == 'Iseult'],
+        'Iseult' : ['{} is Tristan.'.format(player.name) for player in players if player.role == 'Tristan'],
+        'Merlin' : ['{} is Evil'.format(player.name) for player in players if (player.team == 'Evil' and player.role != 'Mordred') or player.role == 'Lancelot'],
+        'Percival' : ['{} is Merlin or Morgana.'.format(player.name) for player in players if player.role == 'Merlin' or player.role == 'Morgana'],
         'Lancelot' : [],
-        'Arthur' : ['{}'.format(player.name) for player in players if player.team is 'Good' and player.role is not 'Arthur'],
+        'Arthur' : ['{}'.format(player.role) for player in players if player.team == 'Good' and player.role != 'Arthur'],
         'Titania' : [],
-        'Nimue' : ['{}'.format(player.name) for player in players if player.role is not 'Nimue']
+        'Nimue' : ['{}'.format(player.role) for player in players if player.role != 'Nimue'],
 
-        'Mordred' : ['{} is Evil.'.format(player.name) for player in players if (player.team is 'Evil' and player is not my_player and player.role is not 'Colgrevance') or player.role is 'Titania'],
-        'Morgana' : ['{} is Evil.'.format(player.name) for player in players if (player.team is 'Evil' and player is not my_player and player.role is not 'Colgrevance') or player.role is 'Titania'],
-        'Maelagant' : ['{} is Evil.'.format(player.name) for player in players if (player.team is 'Evil' and player is not my_player and player.role is not 'Colgrevance') or player.role is 'Titania'],
-        'Agravaine' : ['{} is Evil.'.format(player.name) for player in players if (player.team is 'Evil' and player is not my_player and player.role is not 'Colgrevance') or player.role is 'Titania'],
-        'Colgrevance' : ['{} is {}.'.format(player.name, player.role) for player in players if player.team is 'Evil' and player is not my_player],
-    }.get(my_player.role[])
+        'Mordred' : ['{} is Evil.'.format(player.name) for player in players if (player.team == 'Evil' and player != my_player and player.role != 'Colgrevance') or player.role == 'Titania'],
+        'Morgana' : ['{} is Evil.'.format(player.name) for player in players if (player.team == 'Evil' and player != my_player and player.role != 'Colgrevance') or player.role == 'Titania'],
+        'Maelagant' : ['{} is Evil.'.format(player.name) for player in players if (player.team == 'Evil' and player != my_player and player.role != 'Colgrevance') or player.role == 'Titania'],
+        'Agravaine' : ['{} is Evil.'.format(player.name) for player in players if (player.team == 'Evil' and player != my_player and player.role != 'Colgrevance') or player.role == 'Titania'],
+        'Colgrevance' : ['{} is {}.'.format(player.name, player.role) for player in players if player.team == 'Evil' and player != my_player],
+    }.get(my_player.role,[])
 
 class Player():
     # Players have the following traits
@@ -55,13 +55,13 @@ class Player():
     # type: information or ability
     # seen: a list of what they will see
     # modifier: the random modifier this player has [NOT CURRENTLY UTILIZED]
-    def _init_(self, name)
+    def __init__(self, name):
         self.name = name
         self.role = None
         self.team = None
         self.modifier = None
         self.info = []
-        self.is.assassin = False
+        self.is_assassin = False
 
     def set_role(self, role):
         self.role = role
@@ -75,152 +75,152 @@ class Player():
     def generate_info(self, players):
         pass
 
-    def get_player_info(player_names):
-        num_players = len(player_names)
-        if len(player_names) != num_players:
-            print('ERROR: Duplicate player names.')
-            exit(1)
+def get_player_info(player_names):
+    num_players = len(player_names)
+    if len(player_names) != num_players:
+        print('ERROR: Duplicate player names.')
+        exit(1)
 
-        # create player objects
-        players = []
-        for i in range(0, len(player_names)):
-            player = Player(player_names[i])
-            players.append(player)
+    # create player objects
+    players = []
+    for i in range(0, len(player_names)):
+        player = Player(player_names[i])
+        players.append(player)
 
-        # number of good and evil roles
-        if num_players < 7:
-            num_evil = 2
-        elif num_players < 9:
-            num_evil = 3
+    # number of good and evil roles
+    if num_players < 7:
+        num_evil = 2
+    elif num_players < 9:
+        num_evil = 3
+    else:
+        num_evil = 4
+    num_good = num_players - num_evil
+
+    # establish available roles
+    good_roles = ['Merlin', 'Percival', 'Tristan', 'Iseult', 'Lancelot']
+    evil_roles = ['Mordred', 'Morgana', 'Maelagant']
+
+    # additional roles for player-count
+    # 5 only
+    if num_players > 6:
+        good_roles.append('Nimue')
+
+    # 7 plus
+    if num_players > 6:
+        good_roles.append('Arthur')
+        good_roles.append('Titania')
+
+    # 8 plus
+    if num_players > 7:
+        evil_roles.append('Agravaine')
+
+    # 10 only
+    if num_players == 10:
+        evil_roles.append('Colgrevance')
+
+    '''
+    cide for testing role interaction
+    if num players == 2:
+        good_roles = ['Merlin']
+        evil_roles = ['Maeve']
+        num_good = 1
+        num_evil = 1
+    '''
+    good_roles_in_game = random.sample(good_roles, num_good)
+    evil_roles_in_game = random.sample(evil_roles, num_evil)
+
+    # lone lovers are rerolled
+    # 50% chance to reroll one lone lover
+    # 50% chance to reroll another role into a lover
+    if sum(gr in ['Tristan','Iseult'] for gr in good_roles_in_game) == 1 and num_good > 1:
+        if 'Tristan' in good_roles_in_game:
+            good_roles_in_game.remove('Tristan')
+        if 'Iseult' in good_roles_in_game:
+            good_roles_in_game.remove('Iseult')
+
+        if random.choice([True, False]):
+            # replacing the lone lover
+             available_roles = set(good_roles)-set(good_roles_in_game)-set(['Tristan','Iseult'])
+             good_roles_in_game.append(random.sample(set(available_roles),1)[0])
         else:
-            num_evil = 4
-        num_good = num_players - num_evil
+            # upgradng to pair of lovers
+            rerolled = random.choice(good_roles_in_game)
+            good_roles_in_game.remove(rerolled)
+            good_roles_in_game.append('Tristan')
+            good_roles_in_game.append('Iseult')
 
-        # establish available roles
-        good_roles = ['Merlin', 'Percival', 'Tristan', 'Iseult', 'Lancelot']
-        evil_roles = ['Mordred', 'Morgana', 'Maelagant']
+    # roles after validation
+    #print(good_roles_in_game)
+    #print(evil_roles_in_game)
 
-        # additional roles for player-count
-        # 5 only
-        if num_players > 6:
-            good_roles.append('Nimue')
+    # role assignment
+    random.shuffle(players)
 
-        # 7 plus
-        if num_players > 6:
-            good_roles.append('Arthur')
-            good_roles.append('Titania')
+    good_players = players[:num_good]
+    evil_players = players[num_good:]
 
-        # 8 plus
-        if num_players > 7:
-            evil_roles.append('Agravaine')
+    player_of_role = dict()
 
-        # 10 only
-        if num_players == 10:
-            evil_roles.append('Colgrevance')
+    for gp in good_players:
+        new_role = good_roles_in_game.pop()
+        gp.set_role(new_role)
+        gp.set_team('Good')
+        player_of_role[new_role] = gp
 
-        '''
-        cide for testing role interaction
-        if num players == 2:
-            good_roles = ['Merlin']
-            evil_roles = ['Maeve']
-            num_good = 1
-            num_evil = 1
-        '''
-        good_roles_in_game = random.sample(good_roles, num_good)
-        evil_roles_in_game = random.sample(evil_roles, num_evil)
+    # if there is at least one evil, first evil player becomes assassin
+    if len(evil_players) > 0:
+        evil_players[0].is_assassin = True
 
-        # lone lovers are rerolled
-        # 50% chance to reroll one lone lover
-        # 50% chance to reroll another role into a lover
-        if sum(gr in ['Tristan','Iseult'] for gr in good_roles_in_game) == 1 and num_good > 1:
-            if 'Tristan' in good_roles_in_game:
-                good_roles_in_game.remove('Tristan')
-            if 'Iseult' in good_roles_in_game:
-                good_roles_in_game.remove('Iseult')
+    for ep in evil_players:
+        new_role = evil_roles_in_game.pop()
+        ep.set_role(new_role)
+        ep.set_team('Evil')
+        player_of_role[new_role] = ep
 
-            if random.choice([True, False]):
-                # replacing the lone lover
-                 available_roles = set(good_roles)-set(good_roles_in_game)-set(['Tristan','Iseult'])
-                 good_roles_in_game.append(random.sample(set(available_roles),1)[0])
-            else:
-                # upgradng to pair of lovers
-                rerolled = random.choice(good_roles_in_game)
-                good_roles_in_game.remove(rerolled)
-                good_roles_in_game.append('Tristan')
-                good_roles_in_game.append('Iseult')
+    for p in players:
+        p.add_info(get_role_information(p,players))
+        random.shuffle(p.info)
+        # print(p.name,p.role,p.team,p.info)
 
-        # roles after validation
-        #print(good_roles_in_game)
-        #print(evil_roles_in_game)
+    # Informing Evil about Colgrevance
+    for ep in evil_players:
+        if ep.role != 'Colgrevance' and player_of_role.get('Colgrevance'):
+            ep.add_info(['Colgrevance lurks in the shadows. (There is another Evil that you do not see.)'])
+        if ep.role != 'Colgrevance' and player_of_role.get('Titania'):
+            ep.add_info(['Titania has infiltrated your ranks. (One of the people you see is not Evil.)'])
+        if ep.is_assassin:
+            ep.add_info(['You are the Assassin.'])
 
-        # role assignment
-        random.shuffle(players)
+    # delete and recreate game directory
+    if os.path.isdir("game"):
+        shutil.rmtree("game")
+    os.mkdir("game")
 
-        good_players = players[:num_good]
-        evil_players = players[num_good:]
+    bar= '----------------------------------------\n'
+    for player in players:
+        player.string= bar+'You are '+player.role+' ['+player.team+']\n'+bar+get_role_description(player.role)+'\n'+bar+'\n'.join(player.info)+'\n'+bar
+        player_file = "game/{}".format(player.name)
+        with open(player_file,"w") as file:
+            file.write(player.string)
 
-        player_of_role = dict()
+    first_player = random.sample(players,1)[0]
+    with open("game/start", "w") as file:
+        file.write("The player proposing the first mission is {}.".format(first_player.name))
+        #file.write("\n" + second_mission_starter + " is the starting player of the 2nd round.\n")
 
+    with open("game/DoNotOpen", "w") as file:
+        file.write("Player -> Role\n\n GOOD TEAM:\n")
         for gp in good_players:
-            new_role = good_roles_in_game.pop()
-            gp.set_role(new_role)
-            gp.set_team('Good')
-            player_of_role[new_role] = gp
-
-        # if there is at least one evil, first evil player becomes assassin
-        if len(evil_players) > 0:
-            evil_players[0].is_assassin = True
-            
-        for ep in evil_players:
-            new_role = evil_roles_in_game.pop()
-            ep.set_role(new_role)
-            ep.set_team('Evil')
-            player_of_role[new_role] = ep
-        
-        for p in players:
-            p.add_info(get_role_information(p,players))
-            random.shuffle(p.info)
-            # print(p.name,p.role,p.team,p.info)
-            
-        # Informing Evil about Colgrevance
-        for ep in evil_players:
-            if ep.role is not 'Colgrevance' and player_of_role.get('Colgrevance'):
-                ep.add_info(['Colgrevance lurks in the shadows. (There is another Evil that you do not see.)'])
-            if ep.role is not 'Colgrevance' and player_of_role.get('Titania'):
-                ep.add_info(['Titania has infiltrated your ranks. (One of the people you see is not Evil.)'])
-            if ep.is_assassin:
-                ep.add_info(['You are the Assassin.'])
-        
-        # delete and recreate game directory
-        if os.path.isdir("game"):
-            shutil.rmtree("game")
-        os.mkdir("game")
-        
-        bar= '----------------------------------------\n'
-        for player in players:
-            player.string= bar+'You are '+player.role+' [+player.team+']\n'+bar+get_role_description(player.role)+'/n'+bar+'\n'.join(player.info)+'\n'+bar
-            player_file = "game/{}".format(player.name)
-            with open(player_file,"w") as file:
-                file.write(player.string)
-                
-        first_player = random.sample(players,1)[0]
-        with open("game/start", "w") as file:
-            file.write("The player proposing the first mission is {}.".format(first_player.name))
-            #file.write("\n" + second_mission_starter + " is the starting player of the 2nd round.\n")
-            
-        with open("game/DoNotOpen", "w") as file:
-            file.write("Player -> Role\n\n GOOD TEAM:\n")
-            for gp in good_players:
-                file.write("{} -> {}\n".format(gp.name, gp.role))
+            file.write("{} -> {}\n".format(gp.name, gp.role))
         file.write("\nEVIL TEAM:\n")
         for ep in evil_players:
-                    file.write("{} -> {}\n".format(ep.name,ep.role))
+            file.write("{} -> {}\n".format(ep.name,ep.role))
 
 if __name__ == "__main__":
     if not (6 <= len(sys.argv) <= 11):
         print("Invalid number of players")
         exit(1)
-        
+
     players = sys.argv[1:]
     num_players = len(players)
     players = set(players) # use as a set to avoid duplicate players
@@ -229,5 +229,5 @@ if __name__ == "__main__":
     if len(players) != num_players:
         print("No duplicate player names")
         exit(1)
-        
+
     get_player_info(players)
